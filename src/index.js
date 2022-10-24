04708d38f883a126846aa0e4b61046994d7b219d
Avishka Devinda <68731405+avishka-devinda@users.noreply.github.com>
2022-10-24 17:21:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HelmetProvider } from 'react-helmet-async';
 
 //
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 // ----------------------------------------------------------------------
@@ -20,7 +20,7 @@ root.render(
 );
 
 // If you want to enable client cache, register instead.
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
